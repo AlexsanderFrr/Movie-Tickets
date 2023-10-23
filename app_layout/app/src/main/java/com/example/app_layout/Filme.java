6e06a71868e66085296da3362d04107757d925bb
KedssonFigueredo <kedsson19@gmail.com>
2023-10-22 23:00:21 -0300

    commit da primeira tela

---
+++ app_layout/app/src/main/java/com/example/app_layout/Filme.java
@@ -0,0 +1,68 @@
+package com.example.app_layout;
+
+public class Filme {
+
+    String name;
+    String nPublico;
+    String nTomato;
+    int image;
+    int image_pipoca;
+    int image_tomate;
+
+    public Filme(String name, String nPublico, String nTomato, int image,  int image_pipoca,  int image_tomate) {
+        this.name = name;
+        this.nPublico = nPublico;
+        this.nTomato = nTomato;
+        this.image = image;
+        this.image_pipoca = image_pipoca;
+        this.image_tomate = image_tomate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getnPublico() {
+        return nPublico;
+    }
+
+    public void setnPublico(String nPublico) {
+        this.nPublico = nPublico;
+    }
+
+    public String getnTomato() {
+        return nTomato;
+    }
+
+    public void setnTomato(String nTomato) {
+        this.nTomato = nTomato;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getImage_pipoca() {
+        return image_pipoca;
+    }
+
+    public void setImage_pipoca(int image_pipoca) {
+        this.image_pipoca = image_pipoca;
+    }
+
+    public int getImage_tomate() {
+        return image_tomate;
+    }
+
+    public void setImage_tomate(int image_tomate) {
+        this.image_tomate = image_tomate;
+    }
+}
---
+++ app_layout/app/src/main/java/com/example/app_layout/MyViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.app_layout;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView image_movie, image_tomate, image_pipoca;
+    TextView text_name_movie, text_note_public, text_note_tomato;
+
+
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        image_movie = itemView.findViewById(R.id.image_movie);
+        image_pipoca = itemView.findViewById(R.id.imageView_pipoca);
+        image_tomate = itemView.findViewById(R.id.imageView_tomate);
+        text_name_movie = itemView.findViewById(R.id.text_name_movie);
+        text_note_public = itemView.findViewById(R.id.text_note_public);
+        text_note_tomato = itemView.findViewById(R.id.text_note_tomato);
+
+    }
+}
