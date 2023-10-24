bf4681dad8f9c52c344bfd200c1a2cd4405e8d7e
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2023-10-24 17:16:15 -0300

    mudar cor do assento teste

--- app_layout/app/src/main/java/com/example/app_layout/TelaAssentos.java
+++ app_layout/app/src/main/java/com/example/app_layout/TelaAssentos.java
@@ -2,9 +2,24 @@ package com.example.app_layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaAssentos extends AppCompatActivity {
+    public void trocarBackground(View view) {
+        Drawable backgroundAtual = view.getBackground();
+        Drawable assentoBranco = getResources().getDrawable(R.drawable.assentobranco);
+        Drawable assentoVerde = getResources().getDrawable(R.drawable.assentoverde);
+
+        if (backgroundAtual.getConstantState().equals(assentoBranco.getConstantState())) {
+            // Se o background atual é igual a "assentobranco", troque para "assentoverde"
+            view.setBackground(assentoVerde);
+        } else {
+            // Caso contrário, troque para "assentobranco"
+            view.setBackground(assentoBranco);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
