a9ac5085ed830deb43c6081f5af16b9cdd38fd2d
alexsander <ferreiraalexsander3105@gmail.com>
2023-10-24 19:49:32 -0300

    update tela assentos

--- app_layout/app/src/main/java/com/example/app_layout/TelaAssentos.java
+++ app_layout/app/src/main/java/com/example/app_layout/TelaAssentos.java
@@ -3,11 +3,34 @@ package com.example.app_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class TelaAssentos extends AppCompatActivity {
+
+    public void trocarBackground(View view) {
+        Drawable backgroundAtual = view.getBackground();
+        Drawable assentoBranco = getResources().getDrawable(R.drawable.assentobranco);
+        Drawable assentoVerde = getResources().getDrawable(R.drawable.assentoverde);
+        Drawable assentoEspecial = getResources().getDrawable(R.drawable.assentoespecial);
+        Drawable assentoEspecialVerde = getResources().getDrawable(R.drawable.assentoespecialverde);
+
+        if (backgroundAtual.getConstantState().equals(assentoBranco.getConstantState())) {
+            // Se o background atual é igual a "assentobranco", troque para "assentoverde"
+            view.setBackground(assentoVerde);
+        } else if (backgroundAtual.getConstantState().equals(assentoVerde.getConstantState())) {
+            // Se o background atual é igual a "assentoverde", troque de volta para "assentobranco"
+            view.setBackground(assentoBranco);
+        } else if (backgroundAtual.getConstantState().equals(assentoEspecial.getConstantState())) {
+            // Se o background atual é igual a "assentoespecial", troque para "assentoespecialverde"
+            view.setBackground(assentoEspecialVerde);
+        } else if (backgroundAtual.getConstantState().equals(assentoEspecialVerde.getConstantState())) {
+            // Se o background atual é igual a "assentoespecialverde", troque de volta para "assentoespecial"
+            view.setBackground(assentoEspecial);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
